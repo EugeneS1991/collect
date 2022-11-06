e5468a1b2d725c527ec2db3ea569831bcb89b5df
Yevhen_Skotarenko <test@test.ua>
2022-11-06 12:52:36 +0200

    change app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)

--- app_1.0.py
+++ app_1.0.py
@@ -29,6 +29,7 @@ def save_request(request_id,request):
     req_data['url'] = request.url
     req_data['path'] = request.path
     req_data['remote_addr'] = request.remote_addr
+    print(req_data)
     return req_data
 
 # def save_response(request_id, resp):
@@ -47,17 +48,12 @@ def save_request(request_id,request):
 def after_request(resp):
     cookie = resp.get_json(force=True).get('uuid')
     resp.set_cookie('uuid', value=cookie, max_age=63072000, httponly=True, samesite=None)
-    resp.headers.add('Access-Control-Allow-Origin', '*')
-    resp.headers.add('Access-Control-Allow-Headers', 'Content-Type, X-Token')
-    resp.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    # resp.headers.add('Access-Control-Allow-Origin', '*')
+    # resp.headers.add('Access-Control-Allow-Credentials', True)
     resp.data = {}
     # resp_data = save_response(g.request_id, resp)
     # print('Response:: ', json.dumps(resp_data, indent=4))
     return resp
-
-
-
-
 
 @app.route('/collect',methods=['GET', 'POST'])
 def log():
@@ -68,9 +64,6 @@ def log():
     # Make data for Responce function for responce to website
     resp = Response(json.dumps(req_data, indent=4, default=str), mimetype="application/json")
     return resp
-
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True, debug=False)
