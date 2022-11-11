2b5cf249e23f57936fb550369c5e6a3c3cb0b5ef
Yevhen_Skotarenko <test@test.ua>
2022-11-11 17:56:11 +0200

    add     app.logger.info("my_log_1: ", task_request)     app.logger.info("my_log_2: ",json.dumps(req_data)) and change port to 80

--- app_1.0.py
+++ app_1.0.py
@@ -17,45 +17,54 @@ def save_request(request_id,request):
     uuid = dict(request.cookies).get('uuid')
     if uuid is None:
         req_data['uuid'] = g.request_id
+        # print("is_null:" ,req_data['uuid'])
     else:
         req_data['uuid'] = uuid
+        # print("is_not_null:", req_data['uuid'])
     req_data['timestamp'] = int(time.time_ns() / 1000)
     req_data['data'] = request.data.decode("utf-8")
     req_data['headers'] = dict(request.headers)
-    # req_data['headers'].pop('Cookie', None)
+    req_data['headers'].pop('Cookie', None)
     req_data['args'] = dict(request.args)
     req_data['url'] = request.url
     req_data['path'] = request.path
     req_data['remote_addr'] = request.remote_addr
-    # print(req_data['headers'])
     return req_data
 
-# def save_response(request_id, resp):
-#     resp_data = {}
-#     # resp_data['request_id'] = request_id
-#     # resp_data['uuid'] = resp.headers
-#     # resp_data['status'] = resp.status
-#     resp_data['headers'] = dict(resp.headers)
-#     #resp_data['data'] = dict(resp.get_json())
-#     # resp_data['headers'].get('Set-Cookie')
-#     print(resp_data)
-#     return resp_data
-#
+# @app.before_request
+# def before_request():
+#     print(request.method,
+#           # request.endpoint
+#           )
+#     return request.method
+
+
+def save_response(request_id, resp):
+    resp_data = {}
+    # resp_data['request_id'] = request_id
+    # resp_data['uuid'] = resp.headers
+    # resp_data['status'] = resp.status
+    # resp_data['headers'] = dict(resp.headers)
+    resp_data['data'] = 'data'
+    # resp_data['headers'].get('Set-Cookie')
+    return resp_data
+
 
 @app.after_request
 def after_request(resp):
-    cookie = resp.get_json(force=True).get('uuid')
+    cookie = resp.json.get('uuid')
+    print(cookie)
     resp.set_cookie('uuid', value=cookie, max_age=63072000, httponly=True, samesite=None)
     resp.headers.add('Access-Control-Allow-Origin', '*')
     resp.headers.add('Access-Control-Allow-Credential', True)
-    # resp.data = {}
-    # resp_data = save_response(g.request_id, resp)
-    # print(resp.headers)
+    resp_data = save_response(g.request_id, resp)
+    resp.data = json.dumps(resp_data, indent=4)
     # print('Response:: ', json.dumps(resp_data, indent=4))
     return resp
 
 @app.route('/collect',methods=['GET', 'POST','OPTIONS'])
 def log():
+
     # Create Hesh for every row
     g.request_id = uuid.uuid1().hex
     # Call function and come back data from request in dict type
